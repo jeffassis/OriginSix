96c2e8fc952a2f3bdaf4dbfe3e671fb78bd5dc77
Jeff Assis <jeff-assis@hotmail.com>
2021-06-23 21:47:42 -0300

    Commit Aula04
    Commit sem os itens responsivos

--- main.js
+++ main.js
@@ -18,10 +18,11 @@ for (const link of links) {
 }
 
 // mudar o header da pagina quando der scroll
-const header = document.querySelector('#header')
-const navHeight = header.offsetHeight
 
-window.addEventListener('scroll', function () {
+function changeHeaderWhenScroll() {
+    const header = document.querySelector('#header')
+    const navHeight = header.offsetHeight
+
     if (window.scrollY >= navHeight) {
         // scroll é maior que a altira do header
         header.classList.add('scroll')
@@ -29,7 +30,9 @@ window.addEventListener('scroll', function () {
         // scroll menor que a altura do header
         header.classList.remove('scroll')
     }
-})
+}
+
+
 
 // Testimonials SWIPER JS
 const swiper = new Swiper('.swiper-container', {
@@ -55,6 +58,25 @@ scrollReveal.reveal(
     #about .image, #about .text,
     #services header, #services .card,
     #testimonials header, #testimonials .testimonials,
-    #contact .text, #contact .links
+    #contact .text, #contact .links,
+    footer .brand, footer .social
     `, { interval: 100 }
 )
+
+
+// Botão voltar para o Topo da Pagina
+
+function backToTop() {
+    const backToTopButton = document.querySelector('.back-to-top')
+    if (window.scrollY >= 560) {
+        backToTopButton.classList.add('show')
+    } else {
+        backToTopButton.classList.remove('show')
+    }
+}
+
+// Logica unificada do scroll da pagina
+window.addEventListener('scroll', function () {
+    changeHeaderWhenScroll()
+    backToTop()
+})
